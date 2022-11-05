1e1f22477618d88eccb16f497dc0324d556846d6
Rodrigo Pesenti <RodrigoPesenti55@gmail.com>
2022-11-05 19:39:37 -0300

    Api Integrada

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,130 +1,135 @@
 import { useState } from 'react'
 
-const Formulario = ({ tareas, setTareas }) => {
-//hook use state, posee un array con 2 valores, El useState reicibe como parametros stings, booleanos,enteros, etc
-//Tenemos 4 hooks un por cada campo del formulario
-  const [ nombre, setNombre] = useState('')
-  const [ puesto, setPuesto] = useState('')
-  const [ fecha, setFecha] = useState('')
-  const [ descripcion, setDescripcion] = useState('')
+const Formulario = ({ articulos, setArticulos }) => {
+  //hook use state, posee un array con 2 valores, El useState reicibe como parametros stings, booleanos,enteros, etc
+  //Tenemos 4 hooks un por cada campo del formulario
+  const [descripcion, setDescripcion] = useState('')
+  const [codigo, setCodigo] = useState('')
+  const [costo, setCosto] = useState('')
+  const [posts, setPosts] = useState([]);
 
   //Hook para escuchar los cambios de error
-  const [ error, setError ] = useState(false)
+  const [error, setError] = useState(false)
 
-  //Genera una id que luego llama en un objeto
-  const generarId = () => {
-    const random = Math.random().toString(36).substring(2)
-    const fecha = Date.now().toString(36)
-
-    return random + fecha
+  const crearArticulo = (descripcion,codigo,costo) => {
+    fetch('http://[::1]:3000/articulos', {
+      method: "POST",
+      body: JSON.stringify({
+        descripcion: descripcion,
+        codigo: codigo,
+        costo: costo
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts((posts) => [data, ...posts]);
+        setDescripcion('');
+        setCodigo('');
+        setCosto('');
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }
 
   const handleSubmit = (e) => {
     e.preventDefault() //No ejecuta la funcion hasta que el usuaio oprima el boton
 
     // validacion del Formulario
-    if( [ nombre, puesto, fecha, descripcion].includes('')) {
-      
+    if ([descripcion, codigo, costo].includes('')) {
+
 
       setError(true)
-      
-      
+
+
     } else {
       setError(false)
 
-      // obejeto de tareas
-      const objetoTareas = {
-        nombre,
-        puesto,
-        fecha,
+      // obejeto de articulos
+      const objetoArticulo = {
         descripcion,
-        id: generarId()
+        codigo,
+        costo,
       }
-// ... es un spread operator (Hace una copia de el objeto, para no sobreescribir)
-      setTareas([...tareas, objetoTareas])
 
+      crearArticulo(descripcion,codigo,costo)
+
+      // ... es un spread operator (Hace una copia de el objeto, para no sobreescribir)
+      setArticulos([...articulos, objetoArticulo])
+
+      console.log(descripcion)
       // reiniciar el formulario
-      setNombre('')
-      setPuesto('')
-      setFecha('')
       setDescripcion('')
-
+      setCodigo('')
+      setCosto('')
     }
-    console.log('Enviando Formulario')
+
   }
-    
-    return (
-      <div className="md:w-1/2 lg:w-2/5 mx-5">
-        <h2 className="font-black text-3xl text-center">Seguimiento de tareas</h2>
 
-        <p className="text-lg mt-5 text-center mb-10">
-          Añade tareas y {" "} 
-          <span className="text-indigo-600 font-bold">Administralas</span>
-        </p>
+  return (
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
+      <h2 className="font-black text-3xl text-center">Seguimiento de articulos</h2>
 
-        <form 
-          onSubmit={ handleSubmit }
-          className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <p className="text-lg mt-5 text-center mb-10">
+        Añade articulos y {" "}
+        <span className="text-indigo-600 font-bold">Administralos</span>
+      </p>
 
-          { error && //ternario y //Equivalente a if error then
-          
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+
+        {error && //ternario y //Equivalente a if error then
+
           <div className='bg-red-800 text-white text-center p-3-uppercase font-bold mb-3 rounded-md'>
             <p>Todos los campos son obligatorios </p>
           </div>
           //Si no
-           } 
-            <div className="mb-5">
-              <label htmlFor="Nombre">Nombre del Dev</label>
-              <input
-                id="nombre"
-                className="border-2 w-full p-2 mt-2 placeholder-grey-400 rounded-md"
-                type="text"
-                placeholder="Nombre del desarrollador"
-                value={nombre}
-                //Evento al cual se le pasa una funcion que setea el nombre
-                onChange={(e) => setNombre(e.target.value)}
+        }
+        <div className="mb-5">
+          <label htmlFor="Descripcion">Descripcion</label>
+          <input
+            id="Descripcion"
+            className="border-2 w-full p-2 mt-2 placeholder-grey-400 rounded-md"
+            type="text"
+            placeholder="Descripcion del articulo"
+            value={descripcion}
+            //Evento al cual se le pasa una funcion que setea el nombre
+            onChange={(e) => setDescripcion(e.target.value)}
 
-              />
-            </div>  
+          />
+        </div>
 
-             <div className="mb-5">
-              <label htmlFor="Puesto">Puesto del Dev</label>
-              <input
-                id="puesto"
-                className="border-2 w-full p-2 mt-2 placeholder-grey-400 rounded-md"
-                type="text"
-                placeholder="Puesto del desarrollador"
-                value={puesto}
-                onChange={(e) => setPuesto(e.target.value)}
-              />
-            </div>
+        <div className="mb-5">
+          <label htmlFor="Codigo">Codigo del articulo</label>
+          <input
+            id="codigo"
+            className="border-2 w-full p-2 mt-2 placeholder-grey-400 rounded-md"
+            type="text"
+            placeholder="Codigo del articulo"
+            value={codigo}
+            onChange={(e) => setCodigo(e.target.value)}
+          />
+        </div>
 
-            <div className="mb-5">
-              <label htmlFor="alta">Alta</label>
-              <input
-                id="alta"
-                className="border-2 w-full p-2 mt-2 placeholder-grey-400 rounded-md"
-                type="date"
-                value={fecha}
-                onChange={(e) => setFecha(e.target.value)}
-              />
-            </div>
+        <div className="mb-5">
+          <label htmlFor="costo">Costo del articulo</label>
+          <textarea
+            id="costo"
+            className="border-2 w-full p-2 mt-2 placeholder-grey-400 rounded-md"
+            placeholder="Costo del articulo"
+            value={costo}
+            onChange={(e) => setCosto(parseInt(e.target.value))}
 
-            <div className="mb-5">
-              <label htmlFor="detalle">Descripcion de la tarea</label>
-              <textarea
-                id="detalle"
-                className="border-2 w-full p-2 mt-2 placeholder-grey-400 rounded-md"
-                placeholder="Describe la tarea"
-                value={descripcion}
-                onChange={(e) => setDescripcion(e.target.value)}
+          />
+        </div>
 
-              />
-            </div>
-
-            <input
-              type="submit"
-              className="
+        <input
+          type="submit"
+          className="
               bg-indigo-600 
               w-full 
               p-3 
@@ -132,10 +137,12 @@ const Formulario = ({ tareas, setTareas }) => {
               uppercase 
               font-bold 
               hover:bg-indigo-700 cursor-pointer transition-colors"
-             />
-        </form>
-      </div>
-      );
+        />
+      </form>
+    </div>
+
+  );
 }
- 
+
+
 export default Formulario;
